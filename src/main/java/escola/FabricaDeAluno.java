cdee970a6371b5ef654d50defd404c2a1ade30f8
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-11-12 11:17:29 -0300

    Factories semelhante ao padrao Builder

---
+++ src/main/java/escola/FabricaDeAluno.java
@@ -0,0 +1,23 @@
+package escola;
+
+public class FabricaDeAluno {
+
+    private Aluno aluno;
+
+    public FabricaDeAluno comNomeCPFEmail(String nome, String cpf, String email) {
+        this.aluno = new Aluno(new CPF(cpf), nome, new Email(email));
+
+        return this;
+    }
+
+    public FabricaDeAluno comTelefone(String ddd, String numero) {
+        this.aluno.adicionaTelefone(ddd, numero);
+
+        return this;
+    }
+
+    public Aluno cria() {
+        return this.aluno;
+    }
+
+}
