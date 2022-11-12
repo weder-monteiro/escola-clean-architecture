1ff5633357db699caaba8b13858415587b796803
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-11-12 10:58:13 -0300

    first commit

---
+++ src/test/java/escola/EmailTest.java
@@ -0,0 +1,16 @@
+package escola;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    void naoDeveriaCriarEmailComEnderecoInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> new Email(null));
+        assertThrows(IllegalArgumentException.class, () -> new Email(""));
+        assertThrows(IllegalArgumentException.class, () -> new Email("emailInvalido"));
+    }
+
+}
---
+++ src/test/java/escola/TelefoneTest.java
@@ -0,0 +1,33 @@
+package escola;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TelefoneTest {
+
+    @Test
+    void naoDeveriaCriarTelefoneComDDDInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> new Telefone(null, "123456789"));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("", "123456789"));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("1", "123456789"));
+    }
+
+    @Test
+    void naoDeveriaCriarTelefoneComNumeroInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("11", null));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("11", ""));
+        assertThrows(IllegalArgumentException.class, () -> new Telefone("11", "123"));
+    }
+
+    @Test
+    void deveriaCriarTelefoneComDDDENumeroValidos() {
+        String ddd = "11";
+        String numero = "123456789";
+        Telefone telefone = new Telefone(ddd, numero);
+
+        assertEquals(ddd, telefone.getDdd());
+        assertEquals(numero, telefone.getNumero());
+    }
+
+}
